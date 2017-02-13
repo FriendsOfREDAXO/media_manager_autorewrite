5343506740fe55cbc1d47330732b545aad5f10eb
Hirbod Mirjavadi <info@nightstomp.com>
2017-02-13 12:22:00 +0100

    added class
    Einfache übergabe von $dateiname und $typ, output ist dann der rewrited
    image path

---
+++ lib/mm_autorewrite.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * media_manager_autorewrite Addon.
+ *
+ * @author Friends Of REDAXO
+ *
+ * @var rex_addon
+ */
+class MM_auto
+{
+    public static function rewrite($filename = '', $type = '')
+    {
+        $addon = rex_addon::get('media_manager_autorewrite');
+        $baseTag = $addon->getConfig('is_base_tag');
+        $path = ($baseTag === true) ? '/' : '../';
+
+        $rewrite = $path.'images/'.$type.'/'.$filename;
+
+        return $rewrite;
+    }
+}
